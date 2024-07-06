f78cc044fd8d0da4dbc6108eca9a0f2001825ec4
jali-k <e19210@eng.pdn.ac.lk>
2024-07-07 00:29:14 +0530

    Add data jpa without url

--- src/main/java/com/koda/ecommerce/MyFirstController.java
+++ src/main/java/com/koda/ecommerce/MyFirstController.java
@@ -6,34 +6,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MyFirstController {
 
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "Hello, World!";
-//    }
-
-    @PostMapping("/hello")
-    public String helloPost(@RequestBody String message){
-        return "You just called the POST method at '/hello'!\n: " + message;
-    }
-    @PostMapping("/order")
-    public String helloPost(@RequestBody Order order){
-        return "You just called the POST method at '/hello'!\n: " + order.toString();
-    }
-
-    @PostMapping("/order-record")
-    public String helloPost(@RequestBody OrderRecord order){
-        return "You just called the POST method with order record at /orderRecord!\n: " + order.toString();
-    }
-
-    @GetMapping("/hello/{my-name}")
-    public String helloName(@PathVariable("my-name") String name){
-        return "Hello, " + name + "!";
-    }
-
     @GetMapping("/hello")
-    public String useParam(@RequestParam("my-name") String name){
-        return "Hello, " + name + "!";
+    public String sayHello() {
+        return "Hello, World!";
     }
+
+
+    @PostMapping("/post")
+    public String post(@RequestBody String message){
+        return "You just called the POST method at '/post'!\n: " +message;
+    }
+
 
 }
 
--- src/main/java/com/koda/ecommerce/OrderRecord.java
+++
@@ -1,8 +0,0 @@
-package com.koda.ecommerce;
-
-public record OrderRecord(
-        String name,
-        String product,
-        double price
-) {
-}
